56268793ceeec33662099531f7d17170a3036b81
Justin Lim <goodaccoustics@hotmail.com>
2023-08-21 10:44:08 +0800

    remove whitespaces in user_data section

--- main.tf
+++ main.tf
@@ -19,11 +19,11 @@ resource "aws_instance" "ec2-ansibleserver" {
   subnet_id = "subnet-044b09d27aa26415c"
   vpc_security_group_ids = ["sg-0c3f63d5afd6fa587"]
   user_data = <<EOF
-    #!/bin/bash
-    yum update -y
-    yum install pip -y
-    python3 -m pip install --user ansible
-  EOF
+#!/bin/bash
+yum update -y
+yum install pip -y
+python3 -m pip install --user ansible
+EOF
 
   tags = {
     Name = "justinlim-amazonlinux2023ami-t2.micro-mysimpleminiproject-ansibleserver"
