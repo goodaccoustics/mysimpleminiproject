f82037cbb7af653131173875bb624639b6388b8d
Justin Lim <goodaccoustics@hotmail.com>
2023-08-21 10:25:41 +0800

    update user_data

--- main.tf
+++ main.tf
@@ -20,12 +20,9 @@ resource "aws_instance" "ec2-ansibleserver" {
   vpc_security_group_ids = ["sg-0c3f63d5afd6fa587"]
   user_data = <<EOF
     #!/bin/bash
-    echo "sudo yum update -y"
-    sudo yum update -y
-    echo "sudo yum install pip -y"
-    sudo yum install pip -y
-    echo "sudo python3 -m pip install --user ansible"
-    sudo python3 -m pip install --user ansible
+    yum update -y
+    yum install pip -y
+    python3 -m pip install --user ansible
   EOF
 
   tags = {
