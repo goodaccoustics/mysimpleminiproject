3e6c394b2c11568b8f9caf75ea4c5b18a8a5bdaf
Justin Lim <goodaccoustics@hotmail.com>
2023-08-21 09:35:37 +0800

    put debug echo statements

--- main.tf
+++ main.tf
@@ -20,8 +20,11 @@ resource "aws_instance" "ec2-ansibleserver" {
   vpc_security_group_ids = ["sg-0c3f63d5afd6fa587"]
   user_data = <<EOF
     #!/bin/bash
+    echo "sudo yum update -y"
     sudo yum update -y
+    echo "sudo yum install pip -y"
     sudo yum install pip -y
+    echo "sudo python3 -m pip install --user ansible"
     sudo python3 -m pip install --user ansible
   EOF
 
