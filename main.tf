be3e5c191680b0ce777b2bcc98b717ebd71b9059
goodaccoustics <justinlim@JustinnoMacBook-Air.local>
2023-08-26 11:56:22 +0800

    new backend tfstate location

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "justinlim-backend-bucket"
-    key = "terraform"
+    bucket = "sctp-ce3-tfstate-bkt"
+    key = "justinlim"
     region = "us-east-1"
   }
 }
